0875c5d0f35da22a23f5b9fe2d636fc8f49ea0f3
EddyCharles <eddymaina346@gmail.com>
2026-09-15 01:10:42 -0700

    Add files via upload

---
+++ sem unit.c
@@ -0,0 +1,26 @@
+//PROJECT TO OUTLINE UNITS IN THIS SEMESTER.
+
+/*
+NAME :EDDYCHARLES MAINA
+REG NO:CT100/G/30631/26
+DESCRIPTION :SEMESTER UNIT PROGRAM
+DATE :12/8/2026
+VERSIO:C NO 1
+*/
+
+#include <stdio.h>
+
+int main (){
+	printf("Units offered This semester. \n");
+	printf("1.Introduction to Programming and Algoriths.\n");
+	printf("2.Communication and information Literacy Skills.\n");
+	printf("3.Mathematics for science.\n");
+	printf("4.Essentials of Accounting.\n");
+	printf("5.Computer Systems and Organization.\n");
+	printf("6.Analogue Electronics.\n" );
+	printf("7.Introductions to Computer Applications.\n");
+	printf("8.Fundamentals of Digital Technology and Artificial intelligence.\n");
+	
+	return 0; //execution successful 
+}
+
